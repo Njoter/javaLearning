17ee14ad76b10c0bb95459716730ec8be0866be1
Njoter <63820253+Njoter@users.noreply.github.com>
2022-11-14 02:02:12 +0100

    Update OOP Challenge Heart.java

--- OOP Challenge Heart.java
+++ OOP Challenge Heart.java
@@ -1,12 +1,17 @@
-package com.example.oopchallenge;
+package com.example.oopchallengesolution;
 
 public class Heart extends Organ {
-
     private int heartRate;
 
     public Heart(String name, String medicalCondition, int heartRate) {
         super(name, medicalCondition);
         this.heartRate = heartRate;
+    }
+
+    @Override
+    public void getDetails() {
+        super.getDetails();
+        System.out.println("Heart rate is: " + this.getHeartRate());
     }
 
     public int getHeartRate() {
@@ -15,5 +20,6 @@ public class Heart extends Organ {
 
     public void setHeartRate(int heartRate) {
         this.heartRate = heartRate;
+        System.out.println("Heart rate changed to " + this.getHeartRate());
     }
 }
