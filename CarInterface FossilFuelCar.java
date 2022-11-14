01f15e4883a5ab04ad673f31b344db2b30915dfd
Njoter <63820253+Njoter@users.noreply.github.com>
2022-11-14 23:00:39 +0100

    Create CarInterface FossilFuelCar.java

---
+++ CarInterface FossilFuelCar.java
@@ -0,0 +1,30 @@
+package com.example.interfacesandabstractclasses;
+
+public class FossilFuelCar implements CarInterface {
+
+    private String name;
+
+    public FossilFuelCar(String name) {
+        this.name = name;
+    }
+    
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void printName(String name) {
+
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Explosion in cylinder caused the engine to start");
+    }
+
+    @Override
+    public void move(int speed) {
+        System.out.println(this.getName() + " is moving at " + speed + " mph");
+    }
+}
