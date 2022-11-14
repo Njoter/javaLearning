4bd4e6917edb5c556368fa6e049b682a80608baf
Njoter <63820253+Njoter@users.noreply.github.com>
2022-11-14 23:18:54 +0100

    Create AbstracClass Main.java

---
+++ AbstracClass Main.java
@@ -0,0 +1,8 @@
+package com.example.interfacesandabstractclasses;
+
+public class Main {
+    public static void main(String[] args) {
+        TestAbstractClass testAbstractClass = new TestClass();
+        testAbstractClass.printName("Stian");
+    }
+}
