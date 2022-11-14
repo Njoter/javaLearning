2c29358e1efa3a4621abd0e071f0c93346353b45
Njoter <63820253+Njoter@users.noreply.github.com>
2022-11-14 23:00:04 +0100

    Create CarInterface Main.java

---
+++ CarInterface Main.java
@@ -0,0 +1,13 @@
+package com.example.interfacesandabstractclasses;
+
+public class Main {
+    public static void main(String[] args) {
+        CarInterface carInterface = new ElectricCar("Tesla");
+        carInterface.start();
+        carInterface.move(60);
+
+        CarInterface fossilCarInterface = new FossilFuelCar("Mercedes");
+        fossilCarInterface.start();
+        fossilCarInterface.move(100);
+    }
+}
